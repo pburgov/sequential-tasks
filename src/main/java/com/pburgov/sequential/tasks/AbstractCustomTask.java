6e0bd65d3a272931b050bb7de383251a8e400bee
pburgov <pburgov@gmail.com>
2019-09-02 10:21:56 +0200

    Create AbstractCustomTask closes #1

---
+++ src/main/java/com/pburgov/sequential/tasks/AbstractCustomTask.java
@@ -0,0 +1,54 @@
+package com.pburgov.sequential.tasks;
+
+import javafx.concurrent.Task;
+
+public abstract class AbstractCustomTask <V> extends Task <String> {
+
+    protected static final String TASK_SUCCESS_TEXT = "Success!!";
+    protected static final String TASK_CANCELLED_TEXT = "Cancelled!!";
+    protected Boolean hasLink;
+
+
+    public AbstractCustomTask( String title ) {
+        this.updateTitle(title);
+        this.hasLink = false;
+    }
+
+    @Override
+    abstract protected String call() throws Exception;
+
+    @Override
+    protected void updateMessage( String message ) {
+        super.updateMessage(message);
+    }
+
+    @Override
+    protected void succeeded() {
+        super.succeeded();
+        updateValue(TASK_SUCCESS_TEXT);
+    }
+
+    @Override
+    protected void cancelled() {
+        super.cancelled();
+        updateValue(TASK_CANCELLED_TEXT);
+    }
+
+
+    @Override
+    protected void updateProgress( long workDone, long max ) {
+        super.updateProgress(workDone, max);
+    }
+
+    public Boolean hasLink() {
+        return hasLink;
+    }
+
+    protected void setHasLink( Boolean hasLink ) {
+        this.hasLink = hasLink;
+    }
+
+
+
+
+}
